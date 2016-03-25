8f1cfb08b5cc1484779a8e2fa8e04abdfe2ad971
Calvin Secrest <csecrest2@gmail.com>
2016-03-25 03:35:13 -0400

    Update
    This is the updated and final version of my project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
-/************** controllers ****************/
-/* TodoMain.js: */
 'use strict';
+angular.module("todoListApp", []);
+
+/************** services ****************/
+/* Data.js: */
+'use strict';
+angular.module('todoListApp')
+ .service('dataService', function($http) {
+ 	this.helloConsole = function() {
+
+ 		console.log("this is the hello console service!");
+ 	};
+
+ 	this.getTodos = function(callback){
+ 		$http.get('../navigation-group/todos.json').then(callback)
+
+ 	};
+
+ 	this.deleteTodos = function(todo){
+ 		console.log("The" + todo.name + " todo has been deleted!");
+
+ 	};
+
+ 	this.saveTodos = function(todos){
+ 		console.log(todos.length + " todos have been saved!");
+ 	};
+
+ });
+
+ /************** controllers ****************/
+ /* TodoMain.js: */
+
 angular.module('todoListApp')
  .controller('mainCtrl', function($scope, dataService) {
  	$scope.helloConsole = dataService.helloConsole;
@@ -34,48 +63,50 @@ angular.module('todoListApp')
 
 /************** directive ****************/
 /* Todo.js: */
-angular.module('todoListApp')
- .directive('todos', function(){
-	return {
-		templateUrl: 'templates/todos.html',
-		controller: 'mainCtrl',
-		replace: true
-	}
-
-})
-
-/************** services ****************/
-/* Data.js: */
-'use strict';
-angular.module('todoListApp')
- .service('dataService', function($http) {
- 	this.helloConsole = function() {
-
- 		console.log("this is the hello console service!");
- 	};
-
- 	this.getTodos = function(callback){
- 		$http.get('mock/todos.json').then(callback)
-
- 	};
-
- 	this.deleteTodos = function(todo){
- 		console.log("The" + todo.name + " todo has been deleted!");
-
- 	};
-
- 	this.saveTodos = function(todos){
- 		console.log(todos.length + " todos have been saved!");
- 	};
-
- });
+// angular.module('todoListApp')
+//  .directive('todos', function(){
+// 	return {
+// 		templateUrl: 'templates/todos.html',
+// 		controller: 'mainCtrl',
+// 		replace: true
+// 	}
+//
+// })
 
  /************** Todo App ****************/
  /* app.js: */
- angular.module("todoListApp", [])
+ // angular.module("todoListApp")
+ //
+ // .controller('mainCtrl', function($scope) {
+ //   $scope.helloWorld = function() {
+ //     console.log("Hello there! This the helloWorld controller function, in the mainCtrl!");
+ //   };
+ //   $scope.todos = [
+  //    {"name": "clean the lab"},
+  //    {"name": "finish research paper"},
+  //    {"name": "Project Fermi "},
+  //    {"name": "Project SLAC"},
+  //    {"name": "Tutoring Undergrads"},
+  //    {"name": "Minutes for HEP Meeting"}
+  //  ]
+ //
+ //   dataService.getTodos (function(response) {
+ //     $scope.todos=response.data;
+ //   });
+ // });
 
- .controller('mainCtrl', function($scope) {
-   $scope.helloWorld = function() {
-     console.log("Hello there! This the helloWorld controller function, in the mainCtrl!");
-   };
+ /************** hello-world.js ****************/
+
+ angular.module('todoListApp')
+ .directive('helloWorld', function(){
+
+ 	return{
+ 		template: "This is the hello world directive",
+ 		restrict: "E"
+ 	};
+
  });
+
+ // When defining a module with no module dependencies,
+// the array of dependencies should be defined and empty.
+var myApp = angular.module('myApp', []);
